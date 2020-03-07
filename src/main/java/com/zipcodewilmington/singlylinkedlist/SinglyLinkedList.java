335b78de1263ef9f46830ca9805de9f41d3bc43a
sandeep <snaraya@zipcodes-MBP-9.fios-router.home>
2020-03-06 19:35:46 -0500

    Complete SinglyLinkedList

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,156 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    public Node head = null;
+    public Node tail = null;
+    Integer count = 0;
+
+    public class Node {
+        int data;
+        Node next;
+        Node previous;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.previous = null;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
+    }
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            count = 1;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+            count++;
+        }
+    }
+
+    public Integer size () {
+        return count;
+    }
+
+    public void removeNode (int index) {
+        Node current = head;
+        if(current == null){
+            return;
+        }
+        else if (index == 0){
+            current = current.next;
+            count--;
+        }
+
+        int counter = 0;
+        while (current != null){
+            if(counter == index-1) {
+                current.setNext(current.getNext().getNext());
+                count--;
+                break;
+            }
+
+            current = current.next;
+            counter++;
+
+        }
+    }
+
+    public boolean contains (int data) {
+        Node current = head;
+        while(current != null){
+            if (current.data == data) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public int find (int data) {
+        Node current = head;
+        int counter =0;
+        while (current != null) {
+            if (current.data == data ){
+                return counter;
+            }
+            current = current.next;
+            counter++;
+        }
+        return -1;
+    }
+
+    public int get(int index) {
+        Node current = head;
+        int counter = 0;
+        if (current == null) {
+            return -1;
+        }
+        while (current != null) {
+            if (counter == index) {
+                return current.data;
+            }
+            current = current.next;
+            counter++;
+        }
+        return -1;
+    }
+
+    public SinglyLinkedList copy () {
+        SinglyLinkedList myCopy = new SinglyLinkedList();
+        Node current = this.head;
+        while (current != null) {
+            myCopy.addNode(current.data);
+            current = current.next;
+        }
+        return myCopy;
+    }
+
+    public SinglyLinkedList sort () {
+        Node current = head, nextNode = null;
+        int temp;
+
+        while(current != null){
+            nextNode = current.next;
+            while (nextNode != null){
+                if(current.data > nextNode.data){
+                    temp = current.data;
+                    current.data = nextNode.data;
+                    nextNode.data = temp;
+                }
+                nextNode = nextNode.next;
+            }
+            current = current.next;
+        }
+        return this;
+    }
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,119 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList mylist;
+
+    @Before
+    public void setUp() throws Exception {
+        mylist = new SinglyLinkedList();
+        mylist.addNode(20);
+        mylist.addNode(15);
+        mylist.addNode(18);
+        mylist.addNode(22);
+        mylist.addNode(7);
+    }
+
+    @Test
+    public void addNode() {
+        mylist.addNode(82);
+        Integer expected = 6;
+        Integer actual = mylist.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeNode() {
+        Integer expected = 4;
+        mylist.removeNode(0);
+        Integer actual = mylist.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeNode2() {
+        Integer expected = 4;
+        mylist.removeNode(4);
+        Integer actual = mylist.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeNode3() {
+        mylist.removeNode(1);
+        Assert.assertFalse(mylist.contains(15));
+    }
+
+    @Test
+    public void find() {
+        Integer expected = -1;
+        Integer actual = mylist.find(37);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void find2() {
+        Integer expected = 2;
+        Integer actual = mylist.find(18);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void get() {  //returns the element at the specified index
+        Integer expected = 15;
+        Integer actual = mylist.get(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void get2() {  //returns the element at the specified index
+        Integer expected = -1;
+        Integer actual = mylist.get(8);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void size() {
+        Integer expected = 5;
+        Integer actual = mylist.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void contains() {
+        boolean expected = true;
+        boolean actual = mylist.contains(18);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void copyOf () {
+        Integer expected = mylist.size();
+        Integer actual = mylist.copy().size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sort () {
+        //SinglyLinkedList expected
+        SinglyLinkedList actual = mylist.sort();
+        SinglyLinkedList expected = new SinglyLinkedList();
+        expected.addNode(7);
+        expected.addNode(15);
+        expected.addNode(18);
+        expected.addNode(20);
+        expected.addNode(22);
+        for (int i = 0; i < mylist.size(); i++){
+            Assert.assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+
+
 }
